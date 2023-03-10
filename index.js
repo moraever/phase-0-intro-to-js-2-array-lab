025211711eea63483d923b9bd0788b30eae9c2fe
Ever <ever.m96@hotmail.com>
2023-03-10 04:56:43 -0500

    added and removed elements in an array.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat() {
+    return [cats.push("Ralph")]
+}
+
+function destructivelyPrependCat() {
+    return (cats.unshift("Bob"))
+}
+
+function destructivelyRemoveLastCat() {
+    return (cats.pop())
+}
+
+function destructivelyRemoveFirstCat() {
+    return (cats.shift())
+}
+
+function appendCat() {
+    return ([...cats, "Broom"])
+}
+
+function prependCat() {
+    return(["Arnold", ...cats])
+}
+
+function removeLastCat() {
+    return (cats.slice(0, cats.length - 1))
+}
+
+function removeFirstCat() {
+    return (cats.slice(1))
+}
